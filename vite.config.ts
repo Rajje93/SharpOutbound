f419ddbe1f58046b6a1e0ddeed8f18ce701ac577
Rajje93 <footballrtf@gmail.com>
2025-02-07 02:52:40 +0100

    Fix deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   
   // Optimize dependencies
   optimizeDeps: {
@@ -23,7 +24,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     minify: 'terser',
-    sourcemap: false,
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
